b177b5312fd6dcaa14cf172e715571bef21615bb
l37sg0 <petar_1v@abv.bg>
2022-11-18 11:34:54 +0200

    [#3]  - added examples for InstallData and UpgradeData in Setup.

---
+++ Setup/InstallData.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace L37sg0\HelloWorld\Setup;
+
+use Magento\Framework\Setup\InstallDataInterface;
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+
+class InstallData implements InstallDataInterface
+{
+
+    /**
+     * @inheritDoc
+     */
+    public function install(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        // TODO: Implement install() method.
+    }
+}
---
+++ Setup/UpgradeData.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace L37sg0\HelloWorld\Setup;
+
+use Magento\Framework\Setup\ModuleContextInterface;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+use Magento\Framework\Setup\UpgradeDataInterface;
+
+class UpgradeData implements UpgradeDataInterface
+{
+
+    /**
+     * @inheritDoc
+     */
+    public function upgrade(ModuleDataSetupInterface $setup, ModuleContextInterface $context)
+    {
+        // TODO: Implement upgrade() method.
+    }
+}
