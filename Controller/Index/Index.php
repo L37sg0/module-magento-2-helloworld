7905fbae3d0f9be514f0a04a5232d5958a7aae87
l37sg0 <petar_1v@abv.bg>
2022-11-26 16:10:03 +0200

    [#3]  - retrieving data from table through Controller using factory and model  - Factory Object was auto generated in generated/code/L37sg0/HelloWorld/Model/PostFactory.

--- Controller/Index/Index.php
+++ Controller/Index/Index.php
@@ -2,6 +2,7 @@
 
 namespace L37sg0\HelloWorld\Controller\Index;
 
+use L37sg0\HelloWorld\Model\PostFactory;
 use Magento\Framework\App\Action\Action;
 use Magento\Framework\App\Action\Context;
 use Magento\Framework\View\Result\PageFactory;
@@ -10,9 +11,16 @@ class Index extends Action
 {
     protected $_pageFactory;
 
-    public function __construct(Context $context, PageFactory $pageFactory)
+    protected $_postFactory;
+
+    public function __construct(
+        Context $context,
+        PageFactory $pageFactory,
+        PostFactory $postFactory
+    )
     {
         $this->_pageFactory = $pageFactory;
+        $this->_postFactory = $postFactory;
         return parent::__construct($context);
     }
 
@@ -21,6 +29,15 @@ class Index extends Action
      */
     public function execute()
     {
+        $post = $this->_postFactory->create();
+        $collection = $post->getCollection();
+        foreach ($collection as $item) {
+            echo "<pre>";
+            print_r($item->getData());
+            echo "</pre>";
+        }
+        exit();
+
         return $this->_pageFactory->create();
     }
 }
